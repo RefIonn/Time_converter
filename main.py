326c5a54329f8ed4a41a6c6fd6183f0c0b00ce8b
ItzHaxchaEzz <TrafaretSafary228@yandex.ru>
2023-12-07 18:25:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+from kivy.clock import Clock
+
+
+class Container(BoxLayout):
+    days_input = ObjectProperty()
+
+    hours_output = ObjectProperty()
+    minutes_output = ObjectProperty()
+    seconds_output = ObjectProperty()
+    milliseconds_output = ObjectProperty()
+    weeks_output = ObjectProperty()
+
+    def convert(self):
+        days = int(self.days_input.text)
+        hours = days * 24
+        minutes = days * 1440
+        seconds = minutes * 60
+        milliseconds = seconds * 1000
+        weeks = days/7
+        self.ids.hours_output.text = str(hours)
+        self.ids.minutes_output.text = str(minutes)
+        self.ids.seconds_output.text = str(seconds)
+        self.ids.milliseconds_output.text = str(milliseconds)
+        self.ids.weeks_output.text = str(round(weeks, 2))
+
+
+class MyWin(App):
+    Window.size = (1056, 628)
+
+    def build(self):
+        return Container()
+
+
+if __name__ == '__main__':
+    MyWin().run()
